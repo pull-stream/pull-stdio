ce1b88bec292b33faa8b1afc6480f27b3f331aaf
Jamen Marz <jamenmarz@gmail.com>
2018-01-24 20:09:46 -0700

    Throw when no callback provided (Fixes #3)

--- index.js
+++ index.js
@@ -1,35 +1,29 @@
 var toPull = require('stream-to-pull-stream')
 
+function loud (err) { throw err }
+
 exports.stdin = stdin
 exports.stdout = stdout
 exports.stderr = stderr
 
-/**
- * Handle process.stdin as a pull stream
- * ```js
- * pull(stdin(), ...)
- * ```
- */
 function stdin (options) {
   options = Object.assign({
     encoding: 'utf8'
   }, options || {})
 
-  // Handle `options.stringify`
   if (options.encoding) {
     process.stdin.setEncoding(options.encoding)
   }
 
-  // Turn process.stdin into source pull stream
   return toPull.source(process.stdin)
 }
 
-/**
- * Output stdout/stderr as a pull stream
- * ```js
- * pull(..., stdout())
- * pull(..., stderr())
- * ```
- */
-function stdout (cb) { return toPull.sink(process.stdout, cb) }
-function stderr (cb) { return toPull.sink(process.stderr, cb) }
+function stdout (cb) {
+  if (!cb) cb = loud
+  return toPull.sink(process.stdout, cb)
+}
+
+function stderr (cb) {
+  if (!cb) cb = loud
+  return toPull.sink(process.stderr, cb)
+}
